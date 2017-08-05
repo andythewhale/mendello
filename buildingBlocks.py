a26feba53618bd46998e1342a77db94d4d158a9d
Andy Miller <andymiller1313@gmail.com>
2017-08-05 14:44:12 -0500

    Updates to buildingblocks & Dev book

--- buildingBlocks.py
+++ buildingBlocks.py
@@ -5,63 +5,45 @@ This file is for storing basic building blocks.
 You can pretty much import this file ever time, It shouldn't take up too much memory.
 """
 
-# Defining DNA:
-drA = 'drA'  # Adenine
-drT = 'drT'  # Thymine
-drC = 'drC'  # Cytosine
-drG = 'drG'  # Guanine
-DNA = [drA, drT, drC, drG]  # DNA
+#Defining Nucleic Acids & Stop Codon
 
-# Defining mRNA:
-mrA = 'mrA'  # Adenine
-mrU = 'mrU'  # Uracil
-mrC = 'mrC'  # Cytosine
-mrG = 'mrG'  # Guanine
-mRNA = [mrA, mrU, mrC, mrG]  # mRNA
+#Positive:
+Arg = 'Arg' #Arginine
+His = 'His' #Histadine
+Lys = 'Lys' #Lysine
+Pos = [Arg, His, Lys] #Positive
 
-# Defining tRNA:
-trA = 'trA'  # Adenine
-trU = 'trU'  # Uracil
-trC = 'trC'  # Cytosine
-trG = 'trG'  # Guanine
-tRNA = [trA, trU, trC, trG]  # tRNA
+#Negative:
+Asp = 'Asp' #Aspartic Acid
+Glu = 'Glu' #Glutamic Acid
+Neg = [Asp, Glu] #Negative
 
-# Defining Nucleic Acids:
+#Polar Uncharged:
+Ser = 'Ser' #Serine
+Thr = 'Thr' #Threonine
+Asn = 'Asn' #Asparagine
+Gln = 'Gln' #Glutamine
+Pun = [Ser, Thr, Asn, Gln] #Polar Uncharged
 
-# Positive:
-Arg = 'Arg'  # Arginine
-His = 'His'  # Histadine
-Lys = 'Lys'  # Lysine
-Pos = [Arg, His, Lys]  # Positive
+#Strange
+Cys = 'Cys' #Cysteine
+Sec = 'Sec' #Selenocysteine
+Gly = 'Gly' #Glycine
+Pro = 'Pro' #Proline
+Str = [Cys, Sec, Gly, Pro] #Strange
 
-# Negative:
-Asp = 'Asp'  # Aspartic Acid
-Glu = 'Glu'  # Glutamic Acid
-Neg = [Asp, Glu]  # Negative
-
-# Polar Uncharged:
-Ser = 'Ser'  # Serine
-Thr = 'Thr'  # Threonine
-Asn = 'Asn'  # Asparagine
-Gln = 'Gln'  # Glutamine
-Pun = [Ser, Thr, Asn, Gln]  # Polar Uncharged
-
-# Strange
-Cys = 'Cys'  # Cysteine
-Sec = 'Sec'  # Selenocysteine
-Gly = 'Gly'  # Glycine
-Pro = 'Pro'  # Proline
-Str = [Cys, Sec, Gly, Pro]  # Strange
-
-# Hydrophobic
-Ala = 'Ala'  # Alanine
-Val = 'Val'  # Valine
-Ile = 'Ile'  # Isoleucine
-Leu = 'Leu'  # Leucine
-Met = 'Met'  # Methionine
-Phe = 'Phe'  # Phenylalanine
-Tyr = 'Tyr'  # Tyrosine
-Trp = 'Trp'  # Tryptophan
+#Hydrophobic
+Ala = 'Ala' #Alanine
+Val = 'Val' #Valine
+Ile = 'Ile' #Isoleucine
+Leu = 'Leu' #Leucine
+Met = 'Met' #Methionine
+Phe = 'Phe' #Phenylalanine
+Tyr = 'Tyr' #Tyrosine
+Trp = 'Trp' #Tryptophan
 Hyd = [Ala, Val, Ile, Leu, Met, Phe, Tyr, Trp]
 
-AA = Pos + Neg + Pun + Str + Hyd  # Amino Acids
+#Stop
+Stop = 'Stop'
+
+AA = Pos + Neg + Pun + Str + Hyd #Amino Acids
